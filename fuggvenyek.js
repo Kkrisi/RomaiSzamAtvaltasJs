9934003c9afe99e5fedee7428c15cb4207a49097
Kkrisi <kadarkrisi@gmail.com>
2024-04-24 20:29:44 +0200

    2,3,4 tesztelése is kész

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -41,14 +41,19 @@ export function romaiTeszt() {
     console.log("A teszt életbe lépett!");
 
     console.assert(romaiSzamAtvaltas(1) === "I", "Hiba: 1-et vártunk, de " + romaiSzamAtvaltas(1) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(2) === "II", "Hiba: 2-őt vártunk, de " + romaiSzamAtvaltas(2) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(3) === "III", "Hiba: 3-at vártunk, de " + romaiSzamAtvaltas(3) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(4) === "IV", "Hiba: 4-et vártunk, de " + romaiSzamAtvaltas(4) + " kapunk.");
     console.assert(romaiSzamAtvaltas(5) === "V", "Hiba: 5-öt vártunk, de " + romaiSzamAtvaltas(5) + " kapunk.");
-    console.assert(romaiSzamAtvaltas(8) === "VIII", "Hiba: 8-et vártunk, de " + romaiSzamAtvaltas(8) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(6) === "VI", "Hiba: 6-ot vártunk, de " + romaiSzamAtvaltas(6) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(7) === "VII", "Hiba: 7-et vártunk, de " + romaiSzamAtvaltas(7) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(8) === "VIII", "Hiba: 8-at vártunk, de " + romaiSzamAtvaltas(8) + " kapunk.");
     console.assert(romaiSzamAtvaltas(9) === "IX", "Hiba: 9-et vártunk, de " + romaiSzamAtvaltas(9) + " kapunk.");
 
     console.assert(romaiSzamAtvaltas(10) === "X", "Hiba: 10-et vártunk, de " + romaiSzamAtvaltas(10) + " kapunk.");
     console.assert(romaiSzamAtvaltas(11) === "XI", "Hiba: 11-et vártunk, de " + romaiSzamAtvaltas(11) + " kapunk.");
 
-    console.assert(romaiSzamAtvaltas(33) === "XXXIII", "Hiba: 33-et vártunk, de " + romaiSzamAtvaltas(33) + " kapunk.");
+    console.assert(romaiSzamAtvaltas(33) === "XXXIII", "Hiba: 33-at vártunk, de " + romaiSzamAtvaltas(33) + " kapunk.");
     console.assert(romaiSzamAtvaltas(39) === "XXXIX", "Hiba: 39-et vártunk, de " + romaiSzamAtvaltas(39) + " kapunk.");
     
 
